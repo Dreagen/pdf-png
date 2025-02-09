2caf0c5b0ae0150beec1392bba7ad01976b190bb
Nick Allen <nick.allen@dataglide.co>
2025-02-09 23:33:27 +0000

    Tidy up

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
 use image;
-use std::{env, fs, path::Path};
+use std::{fs, path::Path};
 
 use pdfium_render::prelude::*;
 
+const GREEN: &str = "\x1b[32m";
+const RESET: &str = "\x1b[0m";
+
 fn main() {
-    let current_dir = env::current_dir().expect("Failed to get current directory");
-    println!("Current working directory: {:?}", current_dir);
+    let input_dir_new = Path::new("input/new");
+    fs::create_dir_all(&input_dir_new).unwrap();
 
-    let input_directory = Path::new("input");
-    fs::create_dir_all(&input_directory).expect(&format!(
-        "Could not create input directory {:?}",
-        input_directory
-    ));
+    let input_dir_old = Path::new("input/old");
+    fs::create_dir_all(&input_dir_old).unwrap();
 
-    let input_directory = fs::read_dir(input_directory).expect("couldn't read input directory");
-
-    for (index, dir_entry_result) in input_directory.enumerate() {
+    for dir_entry_result in fs::read_dir(input_dir_new).unwrap() {
         match dir_entry_result {
             Ok(dir_entry) => {
-                println!("{:?}", dir_entry.path());
-                let output_directory = Path::new("output").join(index.to_string());
-                if fs::exists(&output_directory).unwrap() {
-                    fs::remove_dir_all(&output_directory).expect(&format!(
-                        "Could not clear output directory {:?}",
-                        output_directory
-                    ));
+                let output_dir = Path::new("output/new");
+                clear_output_dir(&output_dir);
+                match export_pdf_to_jpegs(
+                    &dir_entry.path(),
+                    &output_dir,
+                    String::from("new"),
+                    Option::None,
+                ) {
+                    Ok(_) => println!("{}Success{}", GREEN, RESET),
+                    Err(err) => println!("failed converting pdf to pngs: {}", err),
                 }
-                match export_pdf_to_jpegs(&dir_entry.path(), &output_directory, Option::None) {
-                    Ok(_) => println!("successfully created images of pfd: {:?}", dir_entry),
+            }
+            Err(error) => println!("Failed to read directory entry {}", error),
+        }
+    }
+
+    for dir_entry_result in fs::read_dir(input_dir_old).unwrap() {
+        match dir_entry_result {
+            Ok(dir_entry) => {
+                let output_dir = Path::new("output/old");
+                clear_output_dir(&output_dir);
+                match export_pdf_to_jpegs(
+                    &dir_entry.path(),
+                    &output_dir,
+                    String::from("old"),
+                    Option::None,
+                ) {
+                    Ok(_) => println!("{}Success{}", GREEN, RESET),
                     Err(err) => println!("failed converting pdf to pngs: {}", err),
                 }
             }
@@ -36,9 +52,19 @@ fn main() {
     }
 }
 
+fn clear_output_dir(output_directory: &std::path::Path) {
+    if fs::exists(&output_directory).unwrap() {
+        fs::remove_dir_all(&output_directory).expect(&format!(
+            "Could not clear output directory {:?}",
+            output_directory
+        ));
+    }
+}
+
 fn export_pdf_to_jpegs(
     pdf_path: &impl AsRef<Path>,
     output_path: &impl AsRef<Path>,
+    output_name: String,
     password: Option<&str>,
 ) -> Result<(), PdfiumError> {
     fs::create_dir_all(&output_path).expect(&format!(
@@ -50,26 +76,22 @@ fn export_pdf_to_jpegs(
         Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./")).unwrap(),
     );
 
-    println!("pdf path: {}", pdf_path.as_ref().display());
-
     let document = pdfium.load_pdf_from_file(pdf_path, password)?;
     let render_config = PdfRenderConfig::new();
 
     for (index, page) in document.pages().iter().enumerate() {
-        let filename = pdf_path
-            .as_ref()
-            .file_stem()
-            .expect("no filename")
-            .to_str()
-            .expect("could not get filename as string");
-
+        println!(
+            "Generating image {} for {}",
+            index + 1,
+            pdf_path.as_ref().display()
+        );
         page.render_with_config(&render_config)?
             .as_image()
             .into_rgb8()
             .save_with_format(
                 output_path
                     .as_ref()
-                    .join(format!("{}-{}.png", filename, index)),
+                    .join(format!("{}-{}.png", output_name, index)),
                 image::ImageFormat::Png,
             )
             .map_err(|e| {
